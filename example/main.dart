9ed10b387cfcbd968232a7037146b120a1cde7a4
Kateryna Kalinchenko <kate.kalinchenko@gmail.com>
2023-08-16 14:13:37 +0300

    Initial commit

---
+++ example/main.dart
@@ -0,0 +1,15 @@
+import 'package:silver_list_extension/silver_list_extension.dart';
+
+void main() {
+  final list = ['q', 'qw', 'aq', 'sq', 'eq'];
+  final emptyList = [];
+
+  print(list.firstWhereOrNull((element) => element[0] == 'a'));
+  print(list.lastWhereOrNull((element) => element[0] == 'b'));
+  print(list.randomOrNull());
+  print(emptyList.randomOrNull());
+  list.replaceWhere((element) => element.contains('q'), '1');
+  print(list);
+  list.replace('1', '12345678');
+  print(list);
+}
---
+++ lib/silver_list_extension.dart
@@ -0,0 +1,51 @@
+library silver_list_extension;
+
+import 'dart:math';
+
+extension SilverListExtension<T> on List<T> {
+  /// Gets the first element that satisfies the given predicate or null.
+  T? firstWhereOrNull(bool Function(T element) test) {
+    for (T element in this) {
+      if (test(element)) return element;
+    }
+
+    return null;
+  }
+
+  /// Gets the last element that satisfies the given predicate or null.
+  T? lastWhereOrNull(bool Function(T element) test) {
+    final lastIndex = lastIndexWhere((element) => false);
+
+    if (lastIndex == -1) {
+      return null;
+    } else {
+      return this[lastIndex];
+    }
+  }
+
+  /// Returns random element.
+  T? randomOrNull() {
+    if (length < 1) {
+      return null;
+    }
+
+    var rnd = Random();
+    final index = rnd.nextInt(length);
+
+    return this[index];
+  }
+
+  /// Replaces the element that matches the predicate.
+  void replaceWhere(bool Function(T element) test, T element) {
+    final index = indexWhere(test);
+    this[index] = element;
+  }
+
+  /// Replaces the element.
+  void replace(T element, T replacement) {
+    final index = indexOf(element);
+    if (index != -1) {
+      this[index] = replacement;
+    }
+  }
+}
